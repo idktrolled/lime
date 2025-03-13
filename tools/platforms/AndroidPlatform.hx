80aaaaef9fb4ad98d9956a3381f8086990603607
Chris Speciale <christopher.gaf@gmail.com>
2025-03-13 08:56:59 -0400

    Android: Set hard minimum sdk ver for `rebuild`
    Avoids expectations of rebuilding clean which could lead to some confusing situations otherwise.

--- tools/platforms/AndroidPlatform.hx
+++ tools/platforms/AndroidPlatform.hx
@@ -373,7 +373,7 @@ class AndroidPlatform extends PlatformTarget
 		var x64 = (command == "rebuild" || ArrayTools.containsValue(project.architectures, Architecture.X64));
 		
 		var commands = [];
-		var minSDKVer = project.config.getInt("android.minimum-sdk-version", 28);
+		var minSDKVer = 28;
 		var platformDefine = '-DPLATFORM_NUMBER=$minSDKVer';
 		
 		if (armv5) commands.push(["-Dandroid", platformDefine]);
