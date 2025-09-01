c8beb396f6f29a03f2021e6f73c3a2b24f77eba3
Regan Green <regan_green27@hotmail.com>
2025-09-01 00:29:58 -0400

    Implement controller rumble support in `Gamepad` (#1739)
    * Add SDL rumble
    
    * Fix rumble
    
    * whitespace fixins real (#2)
    
    * whitespacing real.. please!
    
    * nativecffi rumble thing fix lol
    
    * Remove trailing whitespace.
    
    * Use SDL's argument names and order.
    
    * Standardize formatting.
    
    * Make `SDLGamepad` fully static again, for simplicity.
    
    Also, consistently use `find()` instead of array access, to avoid accidentally creating entries.
    
    Also also, consistently use guard clauses instead of indenting.
    
    * Make another guard clause.
    
    * Update CFFI function signature.
    
    * Use `clamp()` instead of `if` statements.
    
    * Include required header for `std::clamp()`.
    
    * Revert "Use `clamp()` instead of `if` statements."
    
    `std::clamp()` was not available until C++17, and we'd like to continue supporting older versions.
    
    This reverts commit 715a270f79cbed0881eefe980dc4241ac3fb38c8.
    
    * Revert "Include required header for `std::clamp()`."
    
    This reverts commit f47aebf64007e6f8f3d3b24a0492b413abdbeb16.
    
    * Tidy up.
    
    * Document `Gamepad.rumble()`'s arguments.
    
    * Don't limit rumble duration.
    
    SDL apparently supports the full Uint32 range, so there's no reason for Lime to restrict it.
    
    * Fix whitespace.
    
    * Add rumble support in HTML5 (experimental).
    
    ---------
    
    Co-authored-by: Cameron Taylor <cameron.taylor.ninja@gmail.com>
    Co-authored-by: player-03 <player3.14@gmail.com>

--- project/src/ExternalInterface.cpp
+++ project/src/ExternalInterface.cpp
@@ -1765,6 +1765,20 @@ namespace lime {
 	}
 
 
+	void lime_gamepad_rumble (int id, double lowFrequencyRumble, double highFrequencyRumble, int duration) {
+
+		Gamepad::Rumble (id, lowFrequencyRumble, highFrequencyRumble, duration);
+
+	}
+
+
+	HL_PRIM void HL_NAME(hl_gamepad_rumble) (int id, double lowFrequencyRumble, double highFrequencyRumble, int duration) {
+
+		Gamepad::Rumble (id, lowFrequencyRumble, highFrequencyRumble, duration);
+
+	}
+
+
 	value lime_gzip_compress (value buffer, value bytes) {
 
 		#ifdef LIME_ZLIB
@@ -4062,6 +4076,7 @@ namespace lime {
 	DEFINE_PRIME2v (lime_gamepad_event_manager_register);
 	DEFINE_PRIME1 (lime_gamepad_get_device_guid);
 	DEFINE_PRIME1 (lime_gamepad_get_device_name);
+	DEFINE_PRIME4v (lime_gamepad_rumble);
 	DEFINE_PRIME2 (lime_gzip_compress);
 	DEFINE_PRIME2 (lime_gzip_decompress);
 	DEFINE_PRIME2v (lime_haptic_vibrate);
@@ -4255,6 +4270,7 @@ namespace lime {
 	DEFINE_HL_PRIM (_VOID, hl_gamepad_event_manager_register, _FUN(_VOID, _NO_ARG) _TGAMEPAD_EVENT);
 	DEFINE_HL_PRIM (_BYTES, hl_gamepad_get_device_guid, _I32);
 	DEFINE_HL_PRIM (_BYTES, hl_gamepad_get_device_name, _I32);
+	DEFINE_HL_PRIM (_VOID, hl_gamepad_rumble, _I32 _I32 _F64 _F64);
 	DEFINE_HL_PRIM (_TBYTES, hl_gzip_compress, _TBYTES _TBYTES);
 	DEFINE_HL_PRIM (_TBYTES, hl_gzip_decompress, _TBYTES _TBYTES);
 	DEFINE_HL_PRIM (_VOID, hl_haptic_vibrate, _I32 _I32);
